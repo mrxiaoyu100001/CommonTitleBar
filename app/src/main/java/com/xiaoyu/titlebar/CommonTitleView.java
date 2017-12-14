6642c013479a20de8b4f08cdf0bb0bd99b6140cc
xiaoyu <xiaoyu@Nin9ty.local>
2017-12-14 11:22:15 +0800

    new project

---
+++ app/src/main/java/com/xiaoyu/titlebar/CommonTitleView.java
@@ -0,0 +1,33 @@
+package com.xiaoyu.titlebar;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+/**
+ * @Created: xiaoyu  on 2017.12.14 10:54.
+ * @Describe：app通用标题
+ * @Review：
+ * @Modify：
+ * @Version: v_1.0 on 2017.12.14 10:54.
+ * @Blog:http://blog.csdn.net/noteschapter
+ * @Github:https://github.com/mrxiaoyu100001
+ * @Resources:
+ * @Remark:
+ */
+
+public class CommonTitleView extends RelativeLayout {
+
+    public CommonTitleView(Context context) {
+        super(context);
+    }
+
+    public CommonTitleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CommonTitleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
